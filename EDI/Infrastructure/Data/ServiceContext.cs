74ad24e3dfeb6c720015350c35c0b2a877a5446f
rennera <andrew@renner.ca>
2020-08-05 23:11:17 -0400

    Added entities Language and Special Problem

--- EDI/Infrastructure/Data/ServiceContext.cs
+++ EDI/Infrastructure/Data/ServiceContext.cs
@@ -22,15 +22,9 @@ namespace EDI.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Country>()
-                .HasMany(e => e.Provinces)
-                .WithOne(e => e.Country)
-                .HasForeignKey(e => e.CountryID);
+            //modelBuilder.Entity<Country>().HasMany(e => e.Provinces).WithOne(e => e.Country).HasForeignKey(e => e.CountryID);
 
-            modelBuilder.Entity<Country>()
-                .HasMany(e => e.Schools)
-                .WithOne(e => e.Countries)
-                .HasForeignKey(e => e.CountryId);
+            
 
             modelBuilder.Entity<DateDimension>()
                 .Property(e => e.DaySuffix)
@@ -89,7 +83,59 @@ namespace EDI.Infrastructure.Data
             //    .HasMany(e => e.Schools)
             //    .WithOne(e => e.Provinces)
             //    .HasForeignKey(e => e.ProvinceId);
-            
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.ToTable("Countries");
+
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(256);
+
+                entity.Property(e => e.ISO2CountryCode).IsRequired().HasColumnName("ISO2CountryCode").HasMaxLength(2);
+
+                entity.Property(e => e.ISO3CountryCode).IsRequired().HasColumnName("ISO3CountryCode").HasMaxLength(3);
+
+                entity.Property(e => e.ISO3DigitCountry).HasColumnName("ISO3DigitCountry");
+
+                entity.Property(e => e.ModifiedBy).IsRequired().HasMaxLength(256);
+
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                
+                entity.HasMany(e => e.Schools).WithOne(e => e.Countries).HasForeignKey(e => e.CountryId);
+            });
+
+            modelBuilder.Entity<Province>(entity =>
+            {
+                entity.ToTable("Provinces");
+
+                entity.HasIndex(e => e.CountryID);
+
+                entity.HasIndex(e => e.ProvinceTypeId);
+
+                entity.Property(e => e.Code).IsRequired().HasMaxLength(50);
+
+                entity.Property(e => e.CountryID).HasColumnName("CountryID");
+
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(64);
+
+                entity.Property(e => e.ModifiedBy).IsRequired().HasMaxLength(64);
+
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+
+                entity.HasOne(d => d.Country).WithMany(p => p.Provinces).HasForeignKey(d => d.CountryID);
+
+                entity.HasOne(d => d.ProvinceType).WithMany(p => p.Province).HasForeignKey(d => d.ProvinceTypeId);
+            });
+
+            modelBuilder.Entity<ProvinceType>(entity =>
+            {
+                entity.ToTable("ProvinceType");
+
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(64);
+
+                entity.Property(e => e.ModifiedBy).IsRequired().HasMaxLength(64);
+
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+            });
+
             modelBuilder.Entity<Site>(entity =>
             {
                 entity.ToTable("Sites", "EDI");
@@ -128,6 +174,44 @@ namespace EDI.Infrastructure.Data
                 entity.Property(e => e.SchoolName).IsUnicode(false);
 
                 entity.Property(e => e.SchoolNumber).IsUnicode(false);
+                //can be done in this table definition or in foreign key - from here one --> many | from there many --> one
+                //entity.HasOne(d => d.Countries).WithMany(p => p.Schools).HasForeignKey(d => d.CountryId);
+
+                //entity.HasOne(d => d.Sites).WithMany(p => p.Schools).HasForeignKey(d => d.SiteId);
+            });
+
+            modelBuilder.Entity<Language>(entity =>
+            {
+                entity.ToTable("Languages", "EDI");
+
+                //entity.Property(e => e.Id).HasColumnName("ID").ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedBy).IsUnicode(false);
+
+                entity.Property(e => e.LanguageCode).IsRequired().HasMaxLength(3);
+
+                entity.Property(e => e.LanguageEnglish).IsRequired().HasMaxLength(50);
+
+                entity.Property(e => e.LanguageFrench).HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedBy).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<SpecialProblem>(entity =>
+            {
+                entity.ToTable("SpecialProblems", "EDI");
+
+                //entity.Property(e => e.Id).HasColumnName("ID").ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedBy).IsUnicode(false);
+
+                entity.Property(e => e.ModifiedBy).IsUnicode(false);
+
+                entity.Property(e => e.SpecialProblemCode).IsRequired().HasMaxLength(3);
+
+                entity.Property(e => e.SpecialProblemEnglish).HasMaxLength(150);
+
+                entity.Property(e => e.SpecialProblemFrench).HasMaxLength(150);
             });
         }
     }
