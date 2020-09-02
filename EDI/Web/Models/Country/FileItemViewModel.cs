36de05b4ed213df0bba2fde9507ca1b5741adf16
Bryan Deng <helloholiday@outlook.com>
2020-09-02 16:34:25 -0400

    finish index form

---
+++ EDI/Web/Models/Country/FileItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EDI.Web.Models
+{
+    public class FileItemViewModel
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string SiteName { get; set; }
+        public string CoordinatorName { get; set; }
+        public string CoordinatorEmail { get; set; }
+        public string SchoolName { get; set; }
+        public int? SchoolProvinceId { get; set; }
+        public string Province { get; set; }
+        public string TeacherName { get; set; }
+        public string TeacherEmail { get; set; }
+        public string LocalId { get; set; }
+        public byte? ClassTime { get; set; }
+        public int? GenderId { get; set; }
+        public string Gender { get; set; }
+        public DateTime? ChildDob { get; set; }
+        public string ChildPostalCodeZip { get; set; }
+        public string ChildEdiid { get; set; }
+
+        [StringLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        [StringLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
