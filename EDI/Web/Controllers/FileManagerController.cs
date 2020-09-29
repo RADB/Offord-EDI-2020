324371990f8afd7d0b32b2c1b723a53f20eabfba
Bryan Deng <helloholiday@outlook.com>
2020-09-29 13:57:51 -0400

    develop file manager for teachers from different provinces

--- EDI/Web/Controllers/FileManagerController.cs
+++ EDI/Web/Controllers/FileManagerController.cs
@@ -9,21 +9,79 @@ using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
 using Newtonsoft.Json;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using EDI.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using EDI.Web.Extensions;
+using EDI.Infrastructure.Data;
 
 namespace EDI.Web.Controllers
 {
     [Route("api/[controller]")]
     public class FileManagerController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ServiceContext _dbContext;
+        private readonly AppIdentityDbContext _identityContext;
         public PhysicalFileProvider operation;
         public string basePath;
 
         [Obsolete]
-        public FileManagerController(IHostingEnvironment hostingEnvironment)
+        public FileManagerController(IHostingEnvironment hostingEnvironment, 
+            IHttpContextAccessor httpContextAccessor,
+            ServiceContext dbContext,
+            AppIdentityDbContext identityContext,
+            UserManager<ApplicationUser> userManager)
         {
             this.basePath = hostingEnvironment.ContentRootPath;
             this.operation = new PhysicalFileProvider();
-            this.operation.RootFolder(this.basePath + "\\Data\\Files"); // Data\\Files denotes in which files and folders are available.
+
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+            _dbContext = dbContext;
+            _identityContext = identityContext;
+
+            var username = _httpContextAccessor.HttpContext.User.Identity.Name;
+
+            var user = _identityContext.Users.Where(p => p.UserName == username).FirstOrDefault();
+
+            var roleid = _identityContext.UserRoles.Where(p => p.UserId == user.Id).First().RoleId;
+
+            var role = _identityContext.Roles.Where(p => p.Id == roleid).FirstOrDefault();
+
+            var rolename = role.Name;
+
+            string newPath = string.Empty;
+
+            if (rolename == "Administrator" || rolename == "Coordinator")
+            {
+                string folderName = @"Data\Files\";
+                newPath = Path.Combine(this.basePath, folderName);                
+            }
+            else if (rolename == "Teacher")
+            {
+                if (user.ProvinceID.HasValue)
+                {
+                    var province = _dbContext.Provinces.Where(p => p.Id == user.ProvinceID).FirstOrDefault();
+
+                    string folderName = @"Data\Files\" + province.English;
+                    newPath = Path.Combine(this.basePath, folderName);                    
+                }
+                else
+                {
+                    string folderName = @"Data\Files\Unknown";
+                    //string webRootPath = this.basePath + @"\wwwroot";
+                    newPath = Path.Combine(this.basePath, folderName);
+                }
+            }
+
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+
+            this.operation.RootFolder(newPath);
         }
 
         // Processing the File Manager operations
