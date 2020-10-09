d3a5f8fd18f5ed3e0895819abf16a6f8a1fd8672
Bryan Deng <helloholiday@outlook.com>
2020-10-09 01:11:39 -0400

    disable site/school/teacher/year for teacher

--- EDI/Web/Models/Child/ChildItemViewModel.cs
+++ EDI/Web/Models/Child/ChildItemViewModel.cs
@@ -33,6 +33,7 @@ namespace EDI.Web.Models
         public string PostalCodeZip { get; set; }
         public string ChildNumber { get; set; }
         public int? Progress { get; set; }
+        public bool? IsAdmin { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? ModifiedDate { get; set; }
