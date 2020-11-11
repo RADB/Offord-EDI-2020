0401e863c86c371d4e12a4a2a2e6be7dfa0d1f18
rennera <andrew@renner.ca>
2020-11-10 21:45:39 -0500

    Enumerations

---
+++ EDI/Web/Data/Enumerations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDI.Web.Data
+{
+    public class Enumerations
+    {
+        enum Orientation : int
+        {
+            Vertical = 1,
+            Horizontal = 2
+        }
+
+        enum InputType: int
+        {
+            SectionHeader =1,
+            QuestionHeader = 2,
+            Header =3,
+            Text=4,
+            Date=5,
+            Checkbox=6,
+            RadioButtons=7,
+            NumberBox=8,
+            TextArea=9
+        }
+
+    }
+}
