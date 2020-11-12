4215836b24b8f84d93f8e1d6b0e2c45430111f9e
Bryan Deng <helloholiday@outlook.com>
2020-11-11 23:46:50 -0500

    add provinces component

--- EDI/Web/Models/NewsFeed/NewsFeedItemViewModel.cs
+++ EDI/Web/Models/NewsFeed/NewsFeedItemViewModel.cs
@@ -16,6 +16,19 @@ namespace EDI.Web.Models
         public short? Ediyear { get; set; }
         public DateTime? ValidFrom { get; set; }
         public DateTime? ValidTo { get; set; }
+        public bool Alberta { get; set; }
+        public bool BritishColumbia { get; set; }
+        public bool Manitoba { get; set; }
+        public bool NewBrunswick { get; set; }
+        public bool Newfoundland { get; set; }
+        public bool NovaScotia { get; set; }
+        public bool Nunavut { get; set; }
+        public bool Ontario { get; set; }
+        public bool Pei { get; set; }
+        public bool Quebec { get; set; }
+        public bool Saskatchewan { get; set; }
+        public bool Yukon { get; set; }
+        public bool Nwt { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? ModifiedDate { get; set; }
