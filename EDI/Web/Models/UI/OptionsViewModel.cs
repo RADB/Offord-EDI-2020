38ab5269107fe206771fe87ad03d3e46169c13e3
rennera <andrew@renner.ca>
2020-12-03 00:40:19 -0500

    Select Box Part

---
+++ EDI/Web/Models/UI/OptionsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EDI.ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EDI.Web.Models
+{
+    public class OptionsViewModel
+    {
+        public int? ID { get; set; }
+        public string Value { get; set; }
+        public string Text { get; set; }
+        public string Placeholder { get; set; }
+        
+        public List<LookupSetOption> LookupSetOptions;
+
+
+    }
+}
