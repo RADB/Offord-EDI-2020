36ceb5ba70212d7c31a389355f3eb1f9d958adec
Bryan Deng <helloholiday@outlook.com>
2020-12-03 10:22:30 -0500

    get data from TextBox

--- EDI/Web/Models/UI/TextViewModel.cs
+++ EDI/Web/Models/UI/TextViewModel.cs
@@ -5,6 +5,7 @@ namespace EDI.Web.Models
 {
     public class TextViewModel
     {
+        public int? Id { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
         public string Placeholder { get; set; }
