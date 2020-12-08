1c299fc970ed6c17c9016e610c5e3842c7f73c84
rennera <andrew@renner.ca>
2020-12-08 01:00:59 -0500

    Seed Change

--- EDI/Infrastructure/Data/ServiceContextSeed.cs
+++ EDI/Infrastructure/Data/ServiceContextSeed.cs
@@ -2436,6 +2436,7 @@ namespace EDI.Infrastructure.Data
             lus.Add(new LookupSet() { Id = 20, LookupName = "AgeRange", YearId = 1, CreatedBy = "admin", CreatedDate = DateTime.Now, ModifiedBy = "admin", ModifiedDate = DateTime.Now });
             lus.Add(new LookupSet() { Id = 21, LookupName = "TeacherGender", YearId = 1, CreatedBy = "admin", CreatedDate = DateTime.Now, ModifiedBy = "admin", ModifiedDate = DateTime.Now });
             lus.Add(new LookupSet() { Id = 22, LookupName = "VerySomewhatNot", YearId = 1, CreatedBy = "admin", CreatedDate = DateTime.Now, ModifiedBy = "admin", ModifiedDate = DateTime.Now });
+            lus.Add(new LookupSet() { Id = 23, LookupName = "GuideUsefulness", YearId = 1, CreatedBy = "admin", CreatedDate = DateTime.Now, ModifiedBy = "admin", ModifiedDate = DateTime.Now });
 
             return lus;
         }
