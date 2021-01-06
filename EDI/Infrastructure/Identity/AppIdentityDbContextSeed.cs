46e7e37517c7b2806c82dc466429c7cbba0d9ce4
rennera <andrew@renner.ca>
2021-01-06 13:06:53 -0500

    Adding Admin Seeds

--- EDI/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ EDI/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -15,7 +15,9 @@ namespace EDI.Infrastructure.Identity
             }                       
 
             await CreateUserAsync(userManager, "andrew.renner@phri.ca", "EDI&ict2020", "Administrator");
-            await CreateUserAsync(userManager, "bryan.deng@phri.ca", "EDI&ict2020", "Administrator");            
+            await CreateUserAsync(userManager, "bryan.deng@phri.ca", "EDI&ict2020", "Administrator");
+            await CreateUserAsync(userManager, "aofford@mcmaster.ca", "McMaster2021", "Administrator");
+            await CreateUserAsync(userManager, "agaskin@mcmaster.ca", "McMaster2021", "Administrator");
         }
 
         private static async Task CreateUserAsync(UserManager<EDIApplicationUser> userManager, string User, string Password, string Role)
