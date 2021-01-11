e060ace646ec40e5a6209a770758f6b7aa3a1924
rennera <andrew@renner.ca>
2021-01-11 11:21:02 -0500

    Added Errors seeding for Issue 40

--- EDI/Infrastructure/Data/ServiceContextSeed.cs
+++ EDI/Infrastructure/Data/ServiceContextSeed.cs
@@ -2217,6 +2217,8 @@ namespace EDI.Infrastructure.Data
                 new SystemConfigurations() { FieldName = "Fabric Icons", FieldValue = @"https://uifabricicons.azurewebsites.net/", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now },
                 new SystemConfigurations() { FieldName = "Feather Icons", FieldValue = @"https://feathericons.com/", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now },
                 new SystemConfigurations() { FieldName = "Year", FieldValue = @"2021", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now },
+                new SystemConfigurations() { FieldName = "Errors-Email", FieldValue = @"true", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now },
+                new SystemConfigurations() { FieldName = "Errors-Log", FieldValue = @"true", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now },
                 new SystemConfigurations() { FieldName = "DateFormat", FieldValue = @"yyyy-MM-dd", CreatedBy ="rennera", CreatedDate= DateTime.Now, ModifiedBy = "rennera", ModifiedDate = DateTime.Now }
 
 
